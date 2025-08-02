d160afb404924528f550b28376635f7ca96a3107
NguyenTamBao_2251120270_CN22F <brainotaku11@gmail.com>
2025-08-02 18:36:13 +0700

    fist form

---
+++ src/app/(admin)/(others-pages)/profile/page.tsx
@@ -0,0 +1,26 @@
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import UserMetaCard from "@/components/user-profile/UserMetaCard";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Next.js Profile | TailAdmin - Next.js Dashboard Template",
+  description:
+    "This is Next.js Profile page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+};
+
+export default function Profile() {
+  return (
+    <div>
+      <PageBreadcrumb pageTitle="Staffs" />
+      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
+        <div className="space-y-6">
+          <UserMetaCard />
+          <UserMetaCard />
+          <UserMetaCard />
+          <UserMetaCard />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ecommerce/EcommerceMetrics.tsx
@@ -0,0 +1,74 @@
+"use client";
+import React from "react";
+import Badge from "../ui/badge/Badge";
+import { BoxIconLine, GroupIcon } from "@/icons";
+
+export const EcommerceMetrics = () => {
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-6">
+      {/* <!-- Metric Item Start --> */}
+      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+          <GroupIcon />
+        </div>
+
+        <div className="flex items-end justify-between mt-5">
+          <div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Total Projects
+            </span>
+            <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
+              50
+            </h4>
+          </div>
+          <Badge color="success">
+            Projects
+          </Badge>
+        </div>
+      </div>
+      {/* <!-- Metric Item End --> */}
+
+      {/* <!-- Metric Item Start --> */}
+      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+          <BoxIconLine />
+        </div>
+        <div className="flex items-end justify-between mt-5">
+          <div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              On Working Projects
+            </span>
+            <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
+              50
+            </h4>
+          </div>
+          <Badge color="success">
+            Projects
+          </Badge>
+        </div>
+      </div>
+      {/* <!-- Metric Item End --> */}
+
+      {/* <!-- Metric Item Start --> */}
+      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+          <BoxIconLine />
+        </div>
+        <div className="flex items-end justify-between mt-5">
+          <div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Finished Projects
+            </span>
+            <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
+              50
+            </h4>
+          </div>
+          <Badge color="success">
+            Projects
+          </Badge>
+        </div>
+      </div>
+      {/* <!-- Metric Item End --> */}
+    </div>
+  );
+};
